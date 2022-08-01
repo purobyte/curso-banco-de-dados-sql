0ba6fe75ea17f0c9b07dce83f871991091192f30
purobyte <canalpurobyte@gmail.com>
2022-08-01 07:30:35 -0400

    Adicionado resolução de exercicio para video 1

---
+++ aula_01_tabelas_linhas_cols/solucao_exercicio/01_criar_tabela_clima.sql
@@ -0,0 +1,8 @@
+create table clima (
+    pais varchar(80),
+    estado varchar(80),
+    cidade varchar(80),
+    "data" date,
+    temperatura_maxima decimal(5,2),
+    temperatura_minima decimal(5,2)
+);
---
+++ aula_01_tabelas_linhas_cols/solucao_exercicio/02_inserir_dados_clima.sql
@@ -0,0 +1,8 @@
+insert into clima (pais, cidade, estado, "data", temperatura_maxima, temperatura_minima)
+values ('Brasil', 'Rio de Janeiro', 'Rio de Janeiro', '2022-01-01', 40.1, 30.2);
+
+insert into clima (pais, cidade, estado, "data", temperatura_maxima, temperatura_minima)
+values ('Brasil', 'Rio de Janeiro', 'Rio de Janeiro', '2022-01-02', 38.4, 29.7);
+
+insert into clima (pais, cidade, estado, "data", temperatura_maxima, temperatura_minima)
+values ('Brasil', 'Rio de Janeiro', 'Rio de Janeiro', '2022-01-03', 35.2, 26.4);
